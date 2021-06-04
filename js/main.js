abb861e1bf85d7d8e56c1c73ae44930fba9be8aa
ccsbaa <57611957+ccsbaa@users.noreply.github.com>
2021-06-04 16:27:03 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function () {
 
 //* Define spreadsheet URL (make sure you add the #gid=0 for the sheet you want to use)
 //* var googleSheetURI = 'https://docs.google.com/spreadsheets/d/11BNDZeQ4nqwVA-BYViMeM1QfWBtqJT-hqKpc6hh22aM/edit#gid=0';
-var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1baVkGU9D77XZUmpT-g8irMit5i6VDtG4/edit#gid=1318015245';
+var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1hKxvj4_TPQSfiqXtx5DGiNTm7u5H7bTc32Cz8pz7LUE/edit#gid=1318015245';
 //* Compile the Handlebars template for HR leaders
 var HRTemplate = Handlebars.compile($('#hr-template').html());
 
